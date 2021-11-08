4d0d15c709ad21fcb8744ce5ca4a717e2641866a
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-11-08 14:12:32 +0100

    variables init

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//Variables initialization
+const increment = document.querySelector("#increment")
+const save = document.querySelector("#save")
+const people = document.querySelector("#pass-value")
+const logs = document.querySelector("#saved-logs")
+const peopleCount = 0
+
