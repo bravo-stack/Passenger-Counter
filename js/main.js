3e57f93f9da43cf2835f503fe659aec4a248e385
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-11-08 15:49:55 +0100

    added a condition for saving log data; such that logs can't be saved if their value equals zero

--- js/main.js
+++ js/main.js
@@ -16,10 +16,15 @@ increment.addEventListener('click', (e) => {
 
 //add click event listener to save btn
 save.addEventListener('click', (e) => {
+    if(peopleCount === 0) {
+        //do nothing
+        console.log("not allowed to save empty logs")
+    } else {
     const li = document.createElement("li")
     let textNode = document.createTextNode(`${peopleCount}__`)
     li.appendChild(textNode)
     logs.appendChild(li)
     people.innerText = "0"
     peopleCount = 0
+    }
 })
