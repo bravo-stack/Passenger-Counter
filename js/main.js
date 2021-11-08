5bf82ee15b51c0a1ba9cc0dc0f57dbc49e2564d7
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-11-08 15:46:36 +0100

    added click event to the save button, create li tags, added textNode to the li tag, and binded it to the ul

--- js/main.js
+++ js/main.js
@@ -5,9 +5,21 @@ const people = document.querySelector("#pass-value")
 const logs = document.querySelector("#saved-logs")
 let peopleCount = 0
 
+
+
 //add click event listener to increment btn
 increment.addEventListener('click', (e) => {
     peopleCount++;
     people.innerText = `${peopleCount}`
 })
 
+
+//add click event listener to save btn
+save.addEventListener('click', (e) => {
+    const li = document.createElement("li")
+    let textNode = document.createTextNode(`${peopleCount}__`)
+    li.appendChild(textNode)
+    logs.appendChild(li)
+    people.innerText = "0"
+    peopleCount = 0
+})
