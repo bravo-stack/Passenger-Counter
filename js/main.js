a04a4d77a609ace1473338abcdae5122aa65b39e
Bravo Stack <chukwuebukaonuh23@gmail.com>
2021-11-08 14:15:24 +0100

    added click event to increment btn

--- js/main.js
+++ js/main.js
@@ -3,5 +3,11 @@ const increment = document.querySelector("#increment")
 const save = document.querySelector("#save")
 const people = document.querySelector("#pass-value")
 const logs = document.querySelector("#saved-logs")
-const peopleCount = 0
+let peopleCount = 0
+
+//add click event listener to increment btn
+increment.addEventListener('click', (e) => {
+    peopleCount++;
+    people.innerText = `${peopleCount}`
+})
 
